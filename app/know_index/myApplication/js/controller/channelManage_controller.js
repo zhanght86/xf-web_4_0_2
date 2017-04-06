b3ef09b414d365a4f5bbec2a46326f21b064d079
chengjianhua <chengjianhua@ultrapower.com.cn>
2017-04-06 16:07:31 +0800

    添加场景管理页面

--- app/know_index/myApplication/js/controller/channelManage_controller.js
+++ app/know_index/myApplication/js/controller/channelManage_controller.js
@@ -10,12 +10,61 @@
  * 控制器
  */
 
-angular.module('knowledgeManagementModule').controller('channelManageController', [
+angular.module('myApplicationSettingModule').controller('channelManageController', [
     '$scope', 'localStorageService' ,"$state" ,"ngDialog",function ($scope,localStorageService, $state,ngDialog) {
+        setCookie("applicationId","360619411498860544");
+        setCookie("userName","admin1");
         $scope.vm = {
-            addChannel : addChannel,
-            addBlacklist: addBlacklist
+            applicationId: getCookie("applicationId"),
+            channelData : "",   // 渠道数据
+            blackListData : "",  //黑名单数据
+            paginationConf : ""  ,//分页条件
+            pageSize : 5  , //默认每页数量
+            addChannel : addChannel,  //添加渠道
+            addBlacklist: addBlacklist, //添加黑名单
         };
+
+        /**
+         * 加载分页条
+         * @type {{currentPage: number, totalItems: number, itemsPerPage: number, pagesLength: number, perPageOptions: number[]}}
+         */
+        listChannelData(0);
+        //console.log($scope.vm.applicationId)
+        //请求渠道列表
+        function listChannelData(index){
+            httpRequestPost("/api/application/channel/listChannelByPage",{
+                "applicationId": $scope.vm.applicationId,
+                "index" :index==1?0:index,
+                "pageSize": $scope.vm.pageSize
+            },function(data){
+                $scope.vm.channelData = data.data;
+                //console.log(data)
+                $scope.vm.paginationConf = {
+                    currentPage: index,//当前页
+                    totalItems: Math.ceil(data.total/$scope.vm.pageSize), //总页数
+                    pageSize: 1,//每页条目数
+                    pagesLength: 8,//分页框数量
+                };
+                $scope.$apply();
+            },function(){
+                layer.msg("请求失败")
+            })
+        }
+        $scope.$watch('vm.paginationConf.currentPage', function(current){
+            if(current){
+                //console.log(current,$scope.vm.pageSize);
+                httpRequestPost("/api/application/channel/listChannelByPage",{
+                    "applicationId": $scope.vm.applicationId,
+                    "index" :current*$scope.vm.pageSize,
+                    "pageSize": $scope.vm.pageSize
+                },function(){
+                    listChannelData(current);
+                },function(){
+                })
+            }
+        });
+
+
         function addChannel(){
             var dialog = ngDialog.openConfirm({
                 template:"/know_index/myApplication/applicationConfig/channelManageDialog.html",
