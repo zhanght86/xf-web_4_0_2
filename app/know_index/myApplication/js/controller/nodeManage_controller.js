ded209848b3b9b94e39babacc5eea34739406756
chengjianhua <chengjianhua@ultrapower.com.cn>
2017-04-14 11:16:11 +0800

    添加发布服务及编辑服务页面

--- app/know_index/myApplication/js/controller/nodeManage_controller.js
+++ app/know_index/myApplication/js/controller/nodeManage_controller.js
@@ -15,11 +15,39 @@ angular.module('myApplicationSettingModule').controller('nodeManageController', 
             pageSize : 2 , //默认每页数量
             dataTotal: "", //节点数据记录总数
 
+            nodeId : "", //父级节点id
+            nodeAccessIp : "", //父级节点访问地址
+            statusId : "", //父级节点状态
+            nodeType : "", //父级节点类型
+            nodes : [], //新增子集节点集合
+            newNodes : [], //新增子集节点集合
+            subNode : "", //子节点的id
+            subNodeAccessIp : "" , //子节点的访问地址
+            nodeCreateId : getCookie("userId"), //用户id
+
+
+            statusData : "", //节点状态数据
+            typeData : "", //节点类型数据
+
             addNode : addNode, //添加节点
             editNode : editNode, //编辑节点
             disabledAndEnabledNode : disabledAndEnabledNode, //禁用或者启用节点
             deleteNode : deleteNode, //删除节点
+
+            listTypeData : listTypeData, //查询节点类型数据
+            listStatusData : listStatusData, //查询状态数据
+
+            addSubNode : addSubNode, //添加子节点到子节点集合中
+
+            dialogTitle : "",  //对话框标题
+            allowSubmit : 1, //是否允许提交
+            errorTip : "", //错误访问地址提示
+            errorNodeIdTip : "", //错误节点id提示
+
         };
+
+        listTypeData(); //查询节点类型数据
+        listStatusData(); //查询状态数据
 
         /**
          * 加载分页条
@@ -52,7 +80,6 @@ angular.module('myApplicationSettingModule').controller('nodeManageController', 
             }
         });
 
-
         function editNode(){
             var dialog = ngDialog.openConfirm({
                 template:"/know_index/myApplication/applicationRelease/NodeManageDialog.html",
@@ -68,9 +95,7 @@ angular.module('myApplicationSettingModule').controller('nodeManageController', 
             });
         }
 
-
-
-        //添加渠道窗口
+        //添加节点
         function addNode(){
             var dialog = ngDialog.openConfirm({
                 template:"/know_index/myApplication/applicationRelease/NodeManageDialog.html",
@@ -81,30 +106,71 @@ angular.module('myApplicationSettingModule').controller('nodeManageController', 
                 backdrop : 'static',
                 preCloseCallback:function(e){    //关闭回掉
                     if(e === 1){
-                        httpRequestPost("/api/application/channel/addChannel",{
-                            "applicationId": $scope.vm.applicationId,
-                            "channelName": $scope.vm.channelName,
-                            "statusId": $scope.vm.statusId.statusId,
-                            "channelUpdateId": $scope.vm.userId
+                        //angular.forEach($scope.vm.nodes,function(data1){
+                        //    var obj = {};
+                        //    obj.nodeId = data1.nodeId;
+                        //    obj.nodeAccessIp = data1.nodeAccessIp;
+                        //    $scope.vm.newNodes.push(obj);
+                        //});
+                        //console.log($scope.vm.newNodes);
+                        httpRequestPost("/api/application/node/addNode",{
+                            "nodeId" : $scope.vm.nodeId==""?0:$scope.vm.nodeId, //父级节点id
+                            "nodeAccessIp" : $scope.vm.nodeAccessIp, //父级节点访问地址
+                            "statusId" : $scope.vm.statusId, //父级节点状态
+                            "nodeType" : $scope.vm.nodeType, //父级节点类型
+                            "nodes" : $scope.vm.nodes, //新增子集节点集合
+                            "nodeCreateId" : $scope.vm.nodeCreateId //用nodeCreateIdd
                         },function(data){
-                            if(data.data===10002){
-                                layer.msg("渠道重复！");
-                                $scope.vm.channelName = "";
-                            }else{
+                            if(data.status==200){
                                 layer.msg("添加成功");
-                                $state.reload()
+                                $state.reload();
+                            }else{
+                                layer.msg("添加出错了！");
                             }
                         },function(){
                             layer.msg("添加失敗");
-                            $scope.vm.channelName = "";
                         })
                     }else{
                         $scope.vm.channelName = "";
+                        $scope.vm.nodeId=""; //父级节点id
+                        $scope.vm.nodeAccessIp=""; //父级节点访问地址
+                        $scope.vm.nodes=[]; //新增子集节点集合
+                        $scope.vm.subNode=""; //子节点的id
+                        $scope.vm.subNodeAccessIp=""; //子节点的访问地址
                     }
                 }
             });
         }
 
+        //查询节点类型数据
+        function listTypeData(){
+            httpRequestPost("/api/application/node/findNodeType",{
+            },function(data){
+                if(data.status==200){
+                    $scope.vm.typeData=data.data;
+                    $scope.$apply();
+                }else{
+                    layer.msg("查询节点类型失败");
+                }
+            },function(){
+                layer.msg("请求失败");
+            })
+        }
+
+        //查询状态数据
+        function listStatusData(){
+            httpRequestPost("/api/application/node/findNodeStatus",{
+            },function(data){
+                if(data.status==200){
+                    $scope.vm.statusData=data.data;
+                    $scope.$apply();
+                }else{
+                    layer.msg("查询节点类型失败");
+                }
+            },function(){
+                layer.msg("请求失败");
+            })
+        }
 
         //禁用节点
         function disabledAndEnabledNode(nodeCode){
@@ -138,5 +204,94 @@ angular.module('myApplicationSettingModule').controller('nodeManageController', 
                 layer.msg("请求失败");
             })
         }
+
+        //添加子节点信息到子节点集合中并进行校验
+        function addSubNode(nodeId,nodeAccessIp){
+            var obj = {};
+            obj.nodeId = nodeId;
+            obj.nodeAccessIp = nodeAccessIp;
+            if(nodeId!=null&&nodeAccessIp!=null&&nodeId!=""&&nodeAccessIp!=""){
+                if($scope.vm.nodes.inArray(obj)){
+                    layer.msg("重复添加了");
+                }else{
+                    httpRequestPost("/api/application/node/checkNode",{
+                        "nodeId": nodeId,
+                        "nodeAccessIp": nodeAccessIp
+                    },function(data){
+                        if(data.status==200){
+                            $scope.vm.nodes.push(obj);
+                            console.log($scope.vm.nodes);
+                            $scope.vm.subNodeId="";
+                            $scope.vm.subNodeAccessIp="";
+                            $scope.$apply();
+                        }else{
+                            layer.msg(data.info);
+                        }
+                    },function(){
+                        layer.msg("校验失败");
+                    })
+                }
+            }else{
+                layer.msg("节点信息不能为空");
+            }
+            console.log($scope.vm.nodes);
+        }
     }
-]);
+]).directive('checkIp', function($http){
+    return {
+        require: 'ngModel',
+        link: function(scope, ele, attrs, c){
+            scope.$watch(attrs.ngModel, function(n){
+                if(!n) return;
+                $http({
+                    method: 'POST',
+                    url: '/api/application/node/checkNode',
+                    data:{
+                        nodeAccessIp: scope.vm.nodeAccessIp
+                    }
+                }).success(function(data){
+                    if(data.status==200){
+                        c.$setValidity('unique', true);
+                        scope.vm.allowSubmit=1;
+                    }else{
+                        c.$setValidity('unique', false);
+                        scope.vm.allowSubmit=0;
+                        scope.vm.errorTip=data.info;
+                    }
+                }).error(function(data){
+                    c.$setValidity('unique', false);
+                    scope.vm.allowSubmit=0;
+                })
+            });
+        }
+    }
+}).directive('checkNode', function($http){
+    return {
+        require: 'ngModel',
+        link: function(scope, ele, attrs, c){
+            scope.$watch(attrs.ngModel, function(n){
+                if(!n) return;
+                $http({
+                    method: 'POST',
+                    url: '/api/application/node/checkNode',
+                    data:{
+                        nodeId: scope.vm.nodeId,
+                        nodeAccessIp: scope.vm.nodeAccessIp
+                    }
+                }).success(function(data){
+                    if(data.status==200){
+                        c.$setValidity('unique', true);
+                        scope.vm.allowSubmit=1;
+                    }else{
+                        c.$setValidity('unique', false);
+                        scope.vm.allowSubmit=0;
+                        scope.vm.errorNodeIdTip=data.info;
+                    }
+                }).error(function(data){
+                    c.$setValidity('unique', false);
+                    scope.vm.allowSubmit=0;
+                })
+            });
+        }
+    }
+});
