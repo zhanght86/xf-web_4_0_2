32d1db27af63f662c7aefb5825f66bfd0080c92c
mengfei@ultrapower.com.cn <mengfei@ultrapower.com.cn>
2017-04-21 19:05:42 +0800

    Default

--- app/know_index/base/repalace/replace.js
+++ app/know_index/base/repalace/replace.js
@@ -3,13 +3,30 @@
 */
 'use strict';
 
-angular.module('knowledge_static_web').filter('strReplace', function () {
+angular.module('knowledge_static_web').filter('strReplace', ["$sce",function ($sce) {
     return function (value) {
-        return value.replace(/；/g,'，') ;
+      //return value.replace(/；/g,'，') ;
+       return value.split("；");
+        //var result = val.split(",");
+        //function repeat(arr){
+        //    var html;
+        //     angular.forEach(arr,function(item,index){
+        //         if(index%2==1){
+        //             html+='<span class="c_blue">'+item+'</span>,'
+        //         }else{
+        //             //console.log(item);
+        //             html='<span class="c-error">'+item+'</span\>'
+        //         }
+        //     });
+        //    console.log(html);
+        //    //return html
+        //     return $sce.trustAsHtml(html)
+        //}
+        //return repeat(result);
+    };
+}]);
+angular.module('knowledge_static_web').filter('strReplace 2', function () {
+    return function (value) {
+        return value.split(',') ;
     };
 });
-//angular.module('knowledge_static_web').filter('strReplace', function () {
-//    return function (value) {
-//        return value.replace(/，/g,'；') ;
-//    };
-//});
