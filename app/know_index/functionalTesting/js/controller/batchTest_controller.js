a3ef13973f8d435d4ae86ec78c929f22ccf7fe33
niunafang <niunafang@dinfo.cn>
2017-05-24 16:19:13 +0800

    批量上传 删除

--- app/know_index/functionalTesting/js/controller/batchTest_controller.js
+++ app/know_index/functionalTesting/js/controller/batchTest_controller.js
@@ -11,9 +11,43 @@ angular.module('functionalTestModule').controller('batchTestController', [
     function ($scope,localStorageService,$state, $timeout,$stateParams,ngDialog) {
         //$state.go("admin.manage",{userPermission:$stateParams.userPermission});
         $scope.vm = {
-
+            deleteQuestion : deleteQuestion,
+            uploadQuestion : uploadQuestion,
         };
+        function uploadQuestion(callback){
+            var dialog = ngDialog.openConfirm({
+                template: "/know_index/functionalTesting/batchUploadDialog.html",
+                scope: $scope,
+                closeByDocument: false,
+                closeByEscape: true,
+                showClose: true,
+                backdrop: 'static',
+                preCloseCallback: function (e) {    //关闭回掉
+                    if (e === 1) {
 
+                    } else {
+
+                    }
+                }
+            });
+        }
+        function deleteQuestion(callback){
+            var dialog = ngDialog.openConfirm({
+                template: "/know_index/functionalTesting/batchTestDialog.html",
+                scope: $scope,
+                closeByDocument: false,
+                closeByEscape: true,
+                showClose: true,
+                backdrop: 'static',
+                preCloseCallback: function (e) {    //关闭回掉
+                    if (e === 1) {
+
+                    } else {
+
+                    }
+                }
+            });
+        }
         
     }
 ]);
