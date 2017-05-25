6fce661e00664ba396e1175500dd1cab834ed319
rongzihao <rongzihao@ultrapower.com.cn>
2017-05-25 17:21:40 +0800

    会话测试工能完善

--- app/know_index/functionalTesting/js/controller/sessionTest_controller.js
+++ app/know_index/functionalTesting/js/controller/sessionTest_controller.js
@@ -7,13 +7,60 @@
  */
 
 angular.module('functionalTestModule').controller('sessionTestController', [
-    '$scope',"localStorageService","$state","$timeout","$stateParams","ngDialog",
-    function ($scope,localStorageService,$state, $timeout,$stateParams,ngDialog) {
+    '$scope',"localStorageService","$state","$timeout","$stateParams","ngDialog","knowledgeAddServer",
+    function ($scope,localStorageService,$state, $timeout,$stateParams,ngDialog,$cookieStore,knowledgeAddServer) {
         //$state.go("admin.manage",{userPermission:$stateParams.userPermission});
         $scope.vm = {
-
+            pageSize:100,
+            applicationId: $cookieStore.get("applicationId"),
+            userId: $cookieStore.get("userId"),
+            title:"",
+            listService:[],
+            channel:"",
+            channelList : [] ,
+            listDimension:[],
+            //==============================================方法
+            getService:getService,
         };
+        //獲取渠道
+        knowledgeAddServer.getDimensions({ "applicationId" : $scope.vm.applicationId},
+            function(data) {
+                console.log( $scope.vm.applicationId) ;
+                if(data.data){
+                    $scope.vm.dimensions = data.data;
+                    $scope.vm.dimensionsCopy = angular.copy($scope.vm.dimensions);
 
-        
+                }
+            }, function(error) {
+                layer.msg("获取维度失败，请刷新页面")
+            });
+        //获取维度
+        knowledgeAddServer.getChannels({ "applicationId" : $scope.vm.applicationId},
+            function(data) {
+                if(data.data){
+                    $scope.vm.channelList = data.data
+                }
+            }, function(error) {
+                layer.msg("获取渠道失败，请刷新页面")
+            });
+        //页面初始化加载已发布服务
+        getService();
+        function getService(){
+            httpRequestPost("/api/user/listUser",{
+                applicationId:$scope.vm.applicationId,
+            },function(data){
+                if(data.status == 10000){
+                    $scope.vm.listService = data.data;
+                    $scope.$apply()
+                }else if(data.status == 10005) {
+                    layer.msg("当前应用下没有发布服务，请发布服务后进行测试")
+                }
+            },function(){
+                layer.msg("请求失败")
+            })
+        }
+
+        //页面初始化加载维度
+
     }
 ]);
