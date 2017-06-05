25e2700296de74087d8e9716dac8659bf2c1ff7f
mengfei@ultrapower.com.cn <mengfei@ultrapower.com.cn>
2017-06-05 18:00:42 +0800

    default

--- app/app.js
+++ app/app.js
@@ -1008,11 +1008,11 @@ knowledge_static_web
             //})
 
             //初始页
-            .state("index", {
-                url: "/index",
-                templateUrl: 'know_index/index/main.html',
-                controller: "indexController"
-            })
+            //.state("index", {
+            //    url: "/index",
+            //    templateUrl: 'know_index/index/main.html',
+            //    controller: "indexController"
+            //})
 
             //知识接入
             //任务分析
