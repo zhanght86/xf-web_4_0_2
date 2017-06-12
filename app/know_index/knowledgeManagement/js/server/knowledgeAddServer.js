6e47b0e774bbb16572727d2e9929f012b426f572
mengfei@ultrapower.com.cn <mengfei@ultrapower.com.cn>
2017-06-12 14:03:55 +0800

    default

--- app/know_index/knowledgeManagement/js/server/knowledgeAddServer.js
+++ app/know_index/knowledgeManagement/js/server/knowledgeAddServer.js
@@ -7,38 +7,37 @@
     knowledgeAddServer.$injector = ["$resource"];
     function knowledgeAddServer($resource) {
         var services = {
-            getDimensions: getDimensions,  //获取维度
-            getChannels : getChannels ,    //获取渠道
-            getFrame : getFrame ,        //    �@ȡ���
+            getDimensions: getDimensions,  //��ȡ  ����
+            getChannels : getChannels ,    //�@ȡ�S��
+            getFrame : getFrame    ,        //    �@ȡ���
             faqSave : faqSave,
-            conceptGetExtensionByDialogTitle : conceptGetExtensionByDialogTitle ,
-            //getKnowledge : getKnowledge，
-            getDataServer : getDataServer
+            conceptGetExtensionByDialogTitle : conceptGetExtensionByDialogTitle
         };
-        return
-        ;
+        return services;
+
+
         //统一使用 方法
         //url, data, sucCallBack,falCallback, needToken, ajaxType,timeout,timeoutCall
-        function getDataServer(url ,params, onSuccess, onError ,needToken, ajaxType,timeout,timeoutCall) {
-                var url = url;
-                var _resource = $resource(url, {}, {
-                    create: {
-                        method: 'POST',
-                        params: {},
-                        //interceptor: {
-                        //    response: function (d) {
-                        //        console.log(d);
-                        //    },
-                        //    responseError: function (d) {
-                        //        console.log(d);//这里的是随便写的地址，所以执行了error里的函数，可打印看参数及结果
-                        //    }
-                        //}
-                    }
-                });
-                return _resource.create(JSON.stringify(params)).$promise.then(onSuccess, onError);
-        }
+        //function getDataServer(url ,params, onSuccess, onError ,needToken, ajaxType,timeout,timeoutCall) {
+        //    var url = url;
+        //    var _resource = $resource(url, {}, {
+        //        create: {
+        //            method: 'POST',
+        //            params: {},
+                    //interceptor: {
+                    //    response: function (d) {
+                    //        console.log(d);
+                    //    },
+                    //    responseError: function (d) {
+                    //        console.log(d);//这里的是随便写的地址，所以执行了error里的函数，可打印看参数及结果
+                    //    }
+                    //}
+                //}
+            //});
+            //return _resource.create(JSON.stringify(params)).$promise.then(onSuccess, onError);
+        //}
 
-            //function getKnowledge(params, onSuccess, onError) {
+        //function getKnowledge(params, onSuccess, onError) {
         //    var url = '/api/ms/elementKnowledgeAdd/findElementKnowledgeByKnowledgeId';
         //    var _resource = $resource(url, {}, {
         //        create: {
@@ -49,13 +48,12 @@
         //    return _resource.create(JSON.stringify(params)).$promise.then(onSuccess, onError);
         //}
 
-
         function getDimensions(params, onSuccess, onError) {
             var url = '/api/application/dimension/list';
             var _resource = $resource(url, {}, {
                 create: {
                     method: 'POST',
-                    params: {},
+                    params: {}
                 }
             });
             return _resource.create(JSON.stringify(params)).$promise.then(onSuccess, onError);
