a3af7cfcf6ce01075ca849989c75710dab947c2d
mengfei@ultrapower.com.cn <mengfei@ultrapower.com.cn>
2017-06-16 10:23:16 +0800

    统一 用户cookie config

--- app/js/common/config.js
+++ app/js/common/config.js
@@ -3,10 +3,9 @@
  * Time 2017/6/15.
  * INFO 定义公用cookie值
  */
-    //var config = {
-       var  USER_ID = getCookie("userId") ,
-            USER_NAME = getCookie("userName"),
-            APPLICATION_ID = getCookie("applicationId"),
-            APPLICATION_NAME = getCookie("applicationName") ,
-            SCENE_ID = getCookie("sceneId")
-    //} ;
+;var  USER_ID = getCookie("userId") ,
+    USER_NAME = getCookie("userName"),
+    APPLICATION_ID = getCookie("applicationId"),
+    APPLICATION_NAME = getCookie("applicationName") ,
+    SCENE_ID = getCookie("sceneId") ;
+
