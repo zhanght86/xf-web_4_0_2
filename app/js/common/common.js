fe6e81bfcf7b0699c55a2d5a16376881662e488f
shaomingjin <shaomingjin@ultrapower.com.cn>
2017-06-19 12:56:07 +0800

    1、删除多余代码

--- app/js/common/common.js
+++ app/js/common/common.js
@@ -64,26 +64,6 @@ function enterEvent(e,callback) {
         //srcObj.focus()
     }
 }
-/*===================下载文件
- * options:{
- * url:'',  //下载地址
- * data:{name:value}, //要发送的数据
- * method:'post'
- * }
- */
-function DownLoad(options) {
-    var config = $.extend(true, { method: 'post' }, options);
-    var $iframe = $('<iframe id="down-file-iframe" />');
-    var $form = $('<form target="down-file-iframe" method="' + config.method + '" enctype="text/plain" />');
-    $form.attr('action', config.url);
-    for (var key in config.data) {
-        $form.append('<input type="hidden" name="' + key + '" value="' + config.data[key] + '" />');
-    }
-    $iframe.append($form);
-    $(document.body).append($iframe);
-    $form[0].submit();
-    $iframe.remove();
-}
 //请求方式
 function httpRequestPost(url, data, sucCallBack,falCallback, needToken, ajaxType,timeout,timeoutCall) {
     ajaxType == "POST"? "POST" :ajaxType;
