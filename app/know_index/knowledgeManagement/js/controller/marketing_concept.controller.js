9f95a438b2a6e8f76d6a727cf4b57a863bfb0d2b
niunafang <niunafang@dinfo.cn>
2017-06-22 16:17:39 +0800

    营销知识新增添加引导页

--- app/know_index/knowledgeManagement/js/controller/marketing_concept.controller.js
+++ app/know_index/knowledgeManagement/js/controller/marketing_concept.controller.js
@@ -80,7 +80,13 @@ angular.module('knowledgeManagementModule').controller('newConceptController', [
             extensionByContentTitle : [] ,   // 内容生成扩展问 ,
             limitSave : false ,//限制多次打标
             isEdit : false  ,// 知识内容 弹框 编辑  不验证渠道维度重复
-            increaseCheck  : increaseCheck  //知识新增弹框保存按钮
+            increaseCheck  : increaseCheck,  //知识新增弹框保存按钮
+            //引到页
+            showTip : showTip,
+            hideTip : hideTip,
+            prevDiv : prevDiv,
+            nextDiv : nextDiv,
+            //引到页end
         };
         function increaseCheck(){
             if(!$scope.vm.newTitle && !$scope.vm.channel.length){
@@ -1066,6 +1072,47 @@ angular.module('knowledgeManagementModule').controller('newConceptController', [
 
             });
         }
+        //引导页方法
+        function showTip(){
+            $('.shadow_div').show();
+            $('.step_div').show();
+            $('#step_one').show().siblings().hide();
+
+        }
+        function hideTip(){
+            $('.shadow_div').hide();
+            $('.step_div').hide();
+        }
+
+        //上一个
+        function prevDiv(e){
+            var  obj = e.srcElement ? e.srcElement : e.target;
+            if($(obj).parent().parent().parent().prev()){
+                $(obj).parent().parent().parent().hide();
+                $(obj).parent().parent().parent().prev().show();
+                $('html, body').animate({
+                    scrollTop: $(obj).parent().parent().parent().prev().offset().top-20
+                }, 500);
+            }else{
+                // $(obj).attr('disabled',true);
+                return;
+            }
+        }
+        //下一个
+        function nextDiv(e){
+            var  obj = e.srcElement ? e.srcElement : e.target;
+            if($(obj).parent().parent().parent().next()){
+                $(obj).parent().parent().parent().hide();
+                $(obj).parent().parent().parent().next().show();
+                $('html, body').animate({
+                    scrollTop: $(obj).parent().parent().parent().next().offset().top-20
+                }, 500);
+            }else{
+                //$(obj).attr('disabled',true);
+                return;
+            }
+        }
+        //引导页方法end
 
     }
 ]);
