aed2db25143d7f852d7bf58feae1f45d5cdfb405
mengfei@ultrapower.com.cn <mengfei@ultrapower.com.cn>
2017-07-03 14:22:35 +0800

    文檔加工 代碼 统一修改

--- app/know_index/knowledgeManagement/js/controller/document_know_process/doc_select_controller.js
+++ app/know_index/knowledgeManagement/js/controller/document_know_process/doc_select_controller.js
@@ -7,158 +7,150 @@ angular.module('knowledgeManagementModule').controller('docSelectController', [
     "TemplateService","localStorageService","$state","$sce","$cookieStore",
     function ($scope, $location, $stateParams, $interval, $timeout, ngDialog,
               TemplateService,localStorageService,$state,$sce,$cookieStore) {
-        var self = this;
+        $scope.vm = {
+            "roleId" :  $stateParams.roleId ,    //规则ID
+            "temId" : $stateParams.temId  , //模板ID
+            "level" : $stateParams.level , //目录等级
 
-        $scope.userName = $cookieStore.get("userName");
-        $scope.checkedRule = {};
-        $scope.temId = $stateParams.temId;
-        $scope.level = $stateParams.level;
-        $scope.roleId = $stateParams.roleId;
-        $scope.showExtract = false;
-        $scope.checkModel = 1;
-        
-        $scope.queryRole = function(){
+            "checkModel" : 1 ,  //
+
+            "checkedRule" : {} ,        //
+
+            "queryRole" : queryRole , //获取规则
+            "queryTemplateContent" : queryTemplateContent , //获取模板样例
+            "queryTemContent" : "",  // 模板样例
+
+            "titleText" : "" ,
+            "returnRole" : "" ,
+            "extractRegTxt" : "",   //*remove*正则描述
+            "checkedRuleIndex" : 0 ,
+
+            "getSimilarText" : getSimilarText  ,// 根据标题获取相关规则
+            "strs" : "" , //获取的相关内容
+            "saveWordRule" : saveWordRule , // 保存规则
+            "rules" : "" ,
+            "generateRule" : generateRule //标题测试
+        } ;
+        //获取规则
+        function queryRole(){
             TemplateService.queryRuleById.save({
-                ruleId: $scope.roleId
+                ruleId: $scope.vm.roleId
             },function(re){
                 if(re.status == 200){
                     var data = re.data;
-                    $scope.titleText = data.inputText
-                    $scope.extractRegTxt = data.extractReg
-                    $scope.checkModel = data.model
-                    $scope.returnRole = data;
-                    $scope.generateRule();
+                    $scope.vm.titleText = data.inputText ;
+                    $scope.vm.extractRegTxt = data.extractReg ;
+                    $scope.vm.checkModel = data.model ;
+                    $scope.vm.returnRole = data;
+                    generateRule();
                 }
             })
         }
-
-        $scope.queryTemplateContent = function () {
+        //获取模板样例
+         function queryTemplateContent() {
             TemplateService.queryTemplateContent.save({
                 "index": 0,
                 "pageSize": 1,
-               "templateId": $scope.temId
+                "templateId": $scope.vm.temId
             },function(re){
                 if(re.status == 200){
                     if($scope.notEmpty(re.data) && re.data.length > 0){
-                       var  queryTemContent =  $sce.trustAsHtml(re.data);
-                        $scope.queryTemContent = queryTemContent;
+                        $scope.vm.queryTemContent = $sce.trustAsHtml(re.data);
                     }
                 }
             })
         }
 
-        $scope.generateRule = function(){
-            if(!$scope.titleText || $scope.titleText == ''){
+       function generateRule(){
+           console.log($scope.vm.titleText)
+            if(!$scope.vm.titleText || $scope.vm.titleText == ''){
                  layer.msg("请填写匹配标题");
                 return;
             }
             TemplateService.generateRule.save({
-                "templateId":$scope.temId,
-                "level":$scope.level,
-                "text":$scope.titleText
+                "templateId":$scope.vm.temId,
+                "level":$scope.vm.level,
+                "text":$scope.vm.titleText
             },function(re){
                 if(re.status == 200){
                     if(re.data.objs.length > 0){
-                        $scope.rules = re.data.objs;
-                        for(var i=0;i<$scope.rules.length;i++){
-                            var rule = $scope.rules[i]
-                            if($scope.returnRole && $scope.returnRole.firstLineIndent==rule.firstLineIndent && $scope.returnRole.fontAlignment == rule.fontAlignment && $scope.returnRole.selectText == rule.lineWord){
-                                $scope.checkedRuleIndex = i;
+                        $scope.vm.rules = re.data.objs;
+                        for(var i=0;i<$scope.vm.rules.length;i++){
+                            var rule = $scope.vm.rules[i] ;
+                            if($scope.vm.returnRole && $scope.vm.returnRole.firstLineIndent==rule.firstLineIndent && $scope.vm.returnRole.fontAlignment == rule.fontAlignment && $scope.vm.returnRole.selectText == rule.lineWord){
+                                $scope.vm.checkedRuleIndex = i;
                             }
                         }
-                        $scope.getSimilarText();
+                        getSimilarText();
                     }
                 }
             })
         }
-
-        $scope.getSimilarText = function(){
-            if($scope.rules != undefined && $scope.checkedRuleIndex != undefined && $scope.rules[$scope.checkedRuleIndex] != undefined){
-                $scope.checkedRule = $scope.rules[$scope.checkedRuleIndex];
+        //根据标题获取相关规则
+       function getSimilarText(){
+           console.log()
+            if($scope.vm.rules != undefined && $scope.vm.checkedRuleIndex != undefined && $scope.vm.rules[$scope.vm.checkedRuleIndex] != undefined){
+                $scope.vm.checkedRule = $scope.vm.rules[$scope.vm.checkedRuleIndex];
             }else{
-                $scope.checkedRule = undefined;
+                $scope.vm.checkedRule = undefined;
             }
 
-            if(!$scope.checkedRule || $scope.checkedRule == ''){
+            if(!$scope.vm.checkedRule || $scope.vm.checkedRule == ''){
                  layer.msg("请选择匹配标题");
                 return;
             }
-            //console.log($scope.checkedRule)
             TemplateService.getSimilarText.save({
-                "lineWord":$scope.checkedRule.lineWord,
-                "firstLineIndent":$scope.checkedRule.firstLineIndent,
-                "fontAlignment":$scope.checkedRule.fontAlignment,
-                "level":$scope.checkedRule.level,
-                "model":$scope.checkedRule.model,
-                "numFmt":$scope.checkedRule.numFmt,
-                "numLevelText":$scope.checkedRule.numLevelText,
-                "style":$scope.checkedRule.style,
-                "templateId":$scope.checkedRule.templateId
+                "lineWord":$scope.vm.checkedRule.lineWord,
+                "firstLineIndent":$scope.vm.checkedRule.firstLineIndent,
+                "fontAlignment":$scope.vm.checkedRule.fontAlignment,
+                "level":$scope.vm.checkedRule.level,
+                "model":$scope.vm.checkedRule.model,
+                "numFmt":$scope.vm.checkedRule.numFmt,
+                "numLevelText":$scope.vm.checkedRule.numLevelText,
+                "style":$scope.vm.checkedRule.style,
+                "templateId":$scope.vm.checkedRule.templateId
             },function(re){
                 if(re.status == 200){
                     if(re.data.objs.length > 0){
-                        $scope.strs = re.data.objs;
-                        if($scope.extractRegTxt != null && $scope.extractRegTxt != '')
-                            $scope.optimizeText();
+                        $scope.vm.strs = re.data.objs;
+                        //if($scope.vm.extractRegTxt != null && $scope.vm.extractRegTxt != '')
+                        //    $scope.optimizeText();
                     }else{
                          layer.msg("未能抽取到匹配内容");
                     }
                 }
             })
-        }
-        $scope.optimizeText = function(){
-            if(!$scope.strs || $scope.strs.length <= 0){
-                 layer.msg("没有匹配的标题");
-                return;
-            }
-            if(!$scope.extractRegTxt || $scope.extractRegTxt == ''){
-                 layer.msg("请输入正则表达式");
-                return;
-            }
-            TemplateService.optimizeText.save({
-                "regexRule":$scope.extractRegTxt,
-                "texts":$scope.strs
-            },function(re){
-                if(re.status == 200){
-                    if(re.data.objs.length > 0){
-                        $scope.extractStrs = re.data;
-                    }else{
-                         layer.msg("未能匹配到相应正则结果");
-                    }
-                }
-            })
-        }
-
-        $scope.saveWordRule = function(){
-            if($scope.rules != undefined && $scope.checkedRuleIndex != undefined && $scope.rules[$scope.checkedRuleIndex] != undefined){
-                $scope.checkedRule = $scope.rules[$scope.checkedRuleIndex];
+        } ;
+       function saveWordRule(){
+            if($scope.vm.rules != undefined && $scope.vm.checkedRuleIndex != undefined && $scope.vm.rules[$scope.vm.checkedRuleIndex] != undefined){
+                $scope.vm.checkedRule = $scope.vm.rules[$scope.vm.checkedRuleIndex];
             }else{
-                $scope.checkedRule = undefined;
+                $scope.vm.checkedRule = undefined;
             }
 
-            if(!$scope.checkedRule){
+            if(!$scope.vm.checkedRule){
                  layer.msg("请选择要保存的规则");
                 return;
             }
             var params = {
-                "lineWord":$scope.checkedRule.lineWord,
-                "firstLineIndent":$scope.checkedRule.firstLineIndent,
-                "fontAlignment":$scope.checkedRule.fontAlignment,
-                "level":$scope.checkedRule.level,
-                "model":$scope.checkModel,
-                "numFmt":$scope.checkedRule.numFmt,
-                "numLevelText":$scope.checkedRule.numLevelText,
-                "style":$scope.checkedRule.style,
-                "templateId":$scope.checkedRule.templateId,
-                "extractReg":$scope.extractRegTxt,
-                "inputText":$scope.titleText,
-                "selectText":$scope.checkedRule.lineWord,
-                "ruleId":$scope.roleId,
-                "templateCreater": $scope.userName,
-                "templateUpdater": $scope.userName,
+                "lineWord":$scope.vm.checkedRule.lineWord,
+                "firstLineIndent":$scope.vm.checkedRule.firstLineIndent,
+                "fontAlignment":$scope.vm.checkedRule.fontAlignment,
+                "level":$scope.vm.checkedRule.level,
+                "model":$scope.vm.checkModel,
+                "numFmt":$scope.vm.checkedRule.numFmt,
+                "numLevelText":$scope.vm.checkedRule.numLevelText,
+                "style":$scope.vm.checkedRule.style,
+                "templateId":$scope.vm.checkedRule.templateId,
+                "extractReg":$scope.vm.extractRegTxt,
+                "inputText":$scope.vm.titleText,
+                "selectText":$scope.vm.checkedRule.lineWord,
+                "ruleId":$scope.vm.roleId,
+                "templateCreater": USER_NAME,
+                "templateUpdater": USER_NAME,
                 "requestId":"String"
             };
-            if($scope.roleId && $scope.roleId != null && $scope.roleId != "")
+            if($scope.vm.roleId && $scope.vm.roleId != null && $scope.vm.roleId != "")
                 TemplateService.updateWordRule.save(params, function(re){
                     if(re.status == 200){
                         history.back();
@@ -193,19 +185,39 @@ angular.module('knowledgeManagementModule').controller('docSelectController', [
         //         $scope.extractReg = '';
         //     }
         // }
-
-        $scope.checkRule = function(rule){
-            if($scope.returnRole && $scope.returnRole.firstLineIndent==rule.firstLineIndent && $scope.returnRole.fontAlignment == rule.fontAlignment && $scope.returnRole.selectText == rule.lineWord){
-                return true;
-            }else{
-                return false;
-            }
+        queryTemplateContent();
+        if($scope.vm.roleId != null && $scope.vm.roleId != ""){
+            queryRole();
         }
 
-        $scope.queryTemplateContent();
-        if($scope.roleId != null && $scope.roleId != ""){
-            $scope.queryRole();
-        }
+
+        /*
+        *  文档加工正则相关
+        *
+        *  功能删除
+        * */
+        //$scope.optimizeText = function(){
+        //    if(!$scope.vm.strs || $scope.vm.strs.length <= 0){
+        //        layer.msg("没有匹配的标题");
+        //        return;
+        //    }
+        //    if(!$scope.vm.extractRegTxt || $scope.vm.extractRegTxt == ''){
+        //        layer.msg("请输入正则表达式");
+        //        return;
+        //    }
+        //    TemplateService.optimizeText.save({
+        //        "regexRule":$scope.vm.extractRegTxt,
+        //        "texts":$scope.vm.strs
+        //    },function(re){
+        //        if(re.status == 200){
+        //            if(re.data.objs.length > 0){
+        //                $scope.extractStrs = re.data;
+        //            }else{
+        //                layer.msg("未能匹配到相应正则结果");
+        //            }
+        //        }
+        //    })
+        //} ;
         
     }
 ])
