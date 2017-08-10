b88e452830409ac3a204d0a364778614035a6a86
niunafang <niunafang@dinfo.cn>
2017-08-10 13:54:08 +0800

    图文消息

---
+++ app/static/materialManagement/js/controller/addTwMes_controller.js
@@ -0,0 +1,17 @@
+/**
+ * Created by mileS on 2017/7/21.
+ * 控制器
+ */
+angular.module('materialManagement').controller('addTwMesController', [
+    '$scope',"$state","ngDialog", "$cookieStore","$stateParams","$timeout",
+    function ($scope,$state,ngDialog,$cookieStore,$stateParams,$timeout) {
+        $state.go("materialManagement.addtemes");
+        $scope.vm = {
+
+
+        };
+
+
+    }
+]);
+
