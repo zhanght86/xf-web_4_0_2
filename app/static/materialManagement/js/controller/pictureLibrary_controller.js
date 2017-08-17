a439fff21153a84cc8b7444e60712a781ed42bad
zhangjia.wang <wangzhangjia@ultrapower.com.cn>
2017-08-17 17:59:18 +0800

    修改图片

--- app/static/materialManagement/js/controller/pictureLibrary_controller.js
+++ app/static/materialManagement/js/controller/pictureLibrary_controller.js
@@ -15,13 +15,15 @@ angular.module('materialManagement').controller('pictureLibraryController', [
                                 pagesLength: 10,//分页框数量
                              },
             changeName:changeName,
-
+            pictureName:null,
+            napSearch:napSearch,
 
         };
         getPicList(1) ;
         function getPicList(index){
             httpRequestPost("/api/ms/picture/queryPicture",{
                 "applicationId" : APPLICATION_ID,
+                "pictureName" : $scope.vm.pictureName,
                 "index": (index-1)*$scope.vm.paginationConf.pageSize,
                 "pageSize": $scope.vm.paginationConf.pageSize
             },function(data){
@@ -38,6 +40,12 @@ angular.module('materialManagement').controller('pictureLibraryController', [
                 console.log(err)
             }) ;
         }
+
+        function  napSearch(){
+            getPicList(1) ;
+        }
+
+
         function removeImg(item,index){
             layer.confirm('确认删除该图片？', {
                 btn: ['确定','取消'] //按钮
@@ -57,6 +65,21 @@ angular.module('materialManagement').controller('pictureLibraryController', [
                 }) ;
             }, function(){
             });
+        }
+
+        function updateImg(){
+            httpRequestPost("/api/ms/picture/updatePicture",{
+                "pictureName":$scope.vm.pictureName,
+                "pictureId":  $scope.vm.pictureId
+            },function(data){
+                if(data.status == 200){
+                    getPicList(1)
+                }else if(data.status == 500){
+                    layer.msg("名称根寻失败") ;
+                }
+            },function(err){
+                console.log(err)
+            }) ;
         }
         var timeout ;
         $scope.$watch('vm.paginationConf.currentPage', function(current){
@@ -86,7 +109,7 @@ angular.module('materialManagement').controller('pictureLibraryController', [
                 showClose: true,
                 preCloseCallback: function (e) {    //关闭回掉
                     if (e === 1) {
-
+                        updateImg();
                     } else {
 
                     }
