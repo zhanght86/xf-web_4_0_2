183c72c2afc5f150f3b85e1dbaf20df433a28e31
niunafang <niunafang@dinfo.cn>
2017-10-20 15:25:42 +0800

    框架库页面整理，统一页面命名

---
+++ app/static/business_modeling/js/controller/concept_library/_main.controller.js
@@ -0,0 +1,25 @@
+/**
+ * Created by 41212 on 2017/3/23.
+ */
+/**
+ * Created by Administrator on 2016/6/3.
+ * 控制器
+ */
+
+angular.module('businessModelingModule').controller('conceptLibraryController', [
+    '$scope', "$state", "$stateParams",
+    function ($scope,$state, $stateParams) {
+        //$state.go("admin.manage",{userPermission:$stateParams.userPermission});
+        //$scope.vm = {
+        //    isSlide : isSlide,
+        //};
+        //function isSlide(event){
+        //    var self=event.target;
+        //    if($(self).hasClass("slideActive")){
+        //        $(self).removeClass("slideActive").next(".menu_1").stop().slideToggle();
+        //    }else{
+        //        $(self).addClass("slideActive").next(".menu_1").stop().slideToggle();
+        //    }
+        //}
+    }
+]);
