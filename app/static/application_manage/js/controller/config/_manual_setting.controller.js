0d2e2892aa4641b2480c191359d5328cff785842
niunafang <niunafang@dinfo.cn>
2017-11-17 14:33:44 +0800

    转人工设置

--- app/static/application_manage/js/controller/config/_manual_setting.controller.js
+++ app/static/application_manage/js/controller/config/_manual_setting.controller.js
@@ -6,10 +6,18 @@ angular.module('myApplicationSettingModule').controller('manualSettingController
     '$scope', 'localStorageService' ,"ApplicationServer", "$state" ,"ngDialog",'$http', "$cookieStore","$rootScope",
     function ($scope,localStorageService, ApplicationServer ,$state,ngDialog,$http,$cookieStore,$rootScope) {
         $scope.vm = {
-            
-
+            showTip : showTip,
+            hideTip : hideTip,
 
         };
+        function showTip(ev){
+            var event=ev.target;
+            $(event).addClass("on").next("span").show();
+        }
+        function hideTip(ev){
+            var event=ev.target;
+            $(event).removeClass("on").next("span").hide();
+        }
 
     }
 ]);
