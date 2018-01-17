ce7e10afa64f157237a40aeae3fd01803be66b1d
mengfei <mengfei@ultrapower.com.cn>
2018-01-17 17:03:07 +0800

    权限 编辑 参数修改

--- app/static/index/controllers/user_control/_role.controller.js
+++ app/static/index/controllers/user_control/_role.controller.js
@@ -8,23 +8,23 @@ module.exports = homePageModule =>{
     ['$scope',"localStorageService","$state","$timeout","$stateParams","HomePageServer","$location",
     function ($scope,localStorageService, $state,$timeout,$stateParams,HomePageServer,$location) {
         $scope.vm = {
-            roleListDate : [] , //所有角色
+            roleListDate   : [] ,  //所有角色
             paginationConf : {     //分页条件
-                pageSize :$location.search().pageSize?$location.search().pageSize:5 ,
+                pageSize   : $location.search().pageSize?$location.search().pageSize:5 ,
                 currentPage: $location.search().currentPage?$location.search().currentPage:1 ,
-                search : queryRoleList,
-                location : true
+                search     : queryRoleList,
+                location   : true
             }  ,
-            roleName : "" ,
-            description : "" ,
-            addRole : addRole ,
-            deleteRole : deleteRole
+            roleName      : "" ,
+            description   : "" ,
+            addRole       : addRole ,
+            deleteRole    : deleteRole
         }  ;
         let roleHtml = require("../../views/permission_management/dialog_role.html");
         queryRoleList($scope.vm.paginationConf.currentPage,$scope.vm.paginationConf.pageSize) ;
         function queryRoleList(index,pageSize){
             HomePageServer.queryRoleList.save({
-                "index":(index -1)*pageSize,
+                "index"   :(index -1)*pageSize,
                 "pageSize":pageSize
             },function (response) {
                 if(response.status == 200) {
@@ -34,48 +34,42 @@ module.exports = homePageModule =>{
                 }
             })
         }
-        // addRole();
+        // 添加修改角色
         function addRole(roleId,name,description){
             if(roleId){
-              $scope.vm.roleId = roleId ;
               $scope.vm.roleName = name ;
               $scope.vm.description = description ;
             }else{
-                $scope.vm.roleId = "" ;
                 $scope.vm.roleName = "" ;
                 $scope.vm.description = "" ;
             }
             $scope.$parent.$parent.MASTER.openNgDialog($scope,roleHtml,"900px",function(){
-                var zTree = $.fn.zTree.getZTreeObj("treeDemo");
-                //根据过滤机制获得zTree的所有节点
-                let resultNodes = [] ;
-                let nodes = zTree.getNodesByFilter(node=>node);
-                angular.forEach(nodes,function(item,index){
+                let zTree       = $.fn.zTree.getZTreeObj("treeDemo"),  //获取权限树
+                    resultNodes = [] ,                                 //根据过滤机制获得zTree的所有节点
+                    nodes       = zTree.getNodesByFilter(node=>node),  //权限返回节点id
+                    parameter ,apiRes;                                 //参数 & 请求
+                angular.forEach(nodes,function(item,index){           //获取所有选择节点
                     if(item.checked){
                         resultNodes.push(item.id)
                     }
                 }) ;
+                parameter = {
+                        "name"       : $scope.vm.roleName,
+                        "groupList"  : resultNodes,
+                        "description": $scope.vm.description
+                 };
                 if(!roleId){
-                    HomePageServer.addRole.save({
-                        "description": $scope.vm.description,
-                        "groupList": resultNodes,
-                        "name": $scope.vm.roleName
-                    },function (response) {
-                        queryRoleList($scope.vm.paginationConf.currentPage,$scope.vm.paginationConf.pageSize) ;
-                    });
+                    apiRes       = HomePageServer.addRole.save(parameter) ;    //新增角色权限
                 }else{
-                    HomePageServer.updateRole.save({
-                        "id" :  $scope.vm.roleId ,
-                        "description": $scope.vm.description,
-                        "groupList": resultNodes,
-                        "name": $scope.vm.roleName
-                    },function (response) {
-                        queryRoleList($scope.vm.paginationConf.currentPage,$scope.vm.paginationConf.pageSize) ;
-                    });
+                    apiRes       = HomePageServer.updateRole.save(parameter) ; //修改角色权限
+                    parameter.id = roleId ;
                 }
-
-                console.log(nodes);
-                // var permissionList = nodes.filter(nodes)
+                apiRes.$promise.then(function (response) {
+                    if(response.status == 200){
+                        queryRoleList($scope.vm.paginationConf.currentPage,$scope.vm.paginationConf.pageSize) ;
+                    }
+                    layer.msg(response.info)
+                });
             })
         }
         // 删除
@@ -91,11 +85,6 @@ module.exports = homePageModule =>{
                 },function (error) {
                     console.log(error)
                 })
-            },function(){
-                console.log(1)
             });
-
         }
-    }
-])
-};
+}])};
